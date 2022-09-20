e4338d7ebb879f88533db811da5c568521e50190
alemooned <yellow.code@yandex.ru>
2022-09-20 17:21:00 +0300

    HW_5_part2 fix

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -2,7 +2,6 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,7 +46,6 @@ public class MapStorage extends AbstractStorage {
     @Override
     public Resume[] getAll() {
         Resume[] allResume = map.values().toArray(new Resume[0]);
-        Arrays.sort(allResume);
         return allResume;
     }
 
