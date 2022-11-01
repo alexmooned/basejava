4132c342efb89f2d4bd631d43e368f5752f6a6a3
alemooned <yellow.code@yandex.ru>
2022-11-01 11:24:12 +0300

    HW_7 fix

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,17 +1,22 @@
 package com.urise.webapp.model;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Organization {
-    private final Period periods;
+    private final List<Period> periods;
     private final String name;
     private final String website;
 
-    public Organization(Period periods, String name, String website) {
+    public Organization(List<Period> periods, String name, String website) {
         Objects.requireNonNull(name, "name must not be null");
         this.periods = periods;
         this.name = name;
         this.website = website;
+    }
+
+    public List<Period> getPeriods() {
+        return periods;
     }
 
     public String getName() {
