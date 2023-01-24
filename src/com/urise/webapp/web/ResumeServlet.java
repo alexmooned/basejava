b1b79a16dd5353269f9365ed2ee33cf4fee72776
alemooned <yellow.code@yandex.ru>
2023-01-24 21:16:50 +0300

    HW_15 servlets

---
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -0,0 +1,23 @@
+package com.urise.webapp.web;
+
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+    }
+}
